e440443d91327843295594944995dc9dd9eb1a81
Alvaro <alvgaona@gmail.com>
2020-01-18 18:09:08 -0300

    fix: Move definitions to header file.
    Template methods should be declared and defined in the header file
    otherwise CMake would raise an error related to unliked libraries.

--- include/queue_fps.h
+++ include/queue_fps.h
@@ -25,4 +25,48 @@ class QueueFps : private std::queue<T> {
   unsigned int counter_;
 };
 
+template <typename T>
+QueueFps<T>::QueueFps() : counter_(0) {}
+
+template <typename T>
+QueueFps<T>::~QueueFps() {}
+
+template <typename T>
+void QueueFps<T>::Push(const T& entry) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  std::queue<T>::push(entry);
+  counter_++;
+
+  if (counter_ == 1) {
+    tick_meter_.reset();
+    tick_meter_.start();
+  }
+}
+
+template <typename T>
+T QueueFps<T>::Get() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  T entry = this->front();
+  this->pop();
+  return entry;
+}
+
+template <typename T>
+float QueueFps<T>::GetFps() {
+  tick_meter_.stop();
+  double fps = counter_ / tick_meter_.getTimeSec();
+  tick_meter_.start();
+  return static_cast<float>(fps);
+}
+
+template <typename T>
+void QueueFps<T>::Clear() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  while (!this->empty()) this->pop();
+}
+template <typename T>
+bool QueueFps<T>::Empty() {
+  return this->empty();
+}
+
 #endif
